b88d33f8627a35145ae71b0d2b6a7650955a013b
“neilburd” <neilburd@gmail.com>
2016-12-09 14:04:03 -0500

    passing the first few tests

--- lib/arena.rb
+++ lib/arena.rb
@@ -1,2 +1,17 @@
 class Arena
+
+  attr_accessor :name, :gladiators
+
+  def initialize(name)
+    @name = name.capitalize
+    @gladiators = []
+  end
+
+  def add_gladiator
+
+    
+
+  end
+
+
 end
