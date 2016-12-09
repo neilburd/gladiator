abec2001b1570b9942c7a4260eb09a24eacf7da2
“neilburd” <neilburd@gmail.com>
2016-12-09 15:46:53 -0500

    Got it functioning

--- lib/arena.rb
+++ lib/arena.rb
@@ -7,11 +7,45 @@ class Arena
     @gladiators = []
   end
 
-  def add_gladiator
-
-    
-
+  def add_gladiator( gladiators )
+    if @gladiators.length < 2
+      @gladiators.push( gladiators )
+    end
   end
 
+  def fight
+      if @gladiators.empty?
+        return
+      end
+      if @gladiators.count == 2
+          if @gladiators.first.weapon == @gladiators.last.weapon
 
-end
+            @gladiators.pop(2)
+            return
+          end
+
+          if @gladiators[0].weapon == ("Trident")
+            if @gladiators[1].weapon == ("Spear")
+              @gladiators.delete_at(1)
+            elsif @gladiators[1].weapon == ("Club")
+              @gladiators.delete_at(0)
+            end
+          elsif @gladiators[0].weapon == ("Spear")
+            if @gladiators[1].weapon == ("Trident")
+              @gladiators.delete_at(0)
+            elsif @gladiators[1].weapon == ("Club")
+              @gladiators.delete_at(1)
+            end
+          elsif @gladiators[0].weapon == ( "Club" )
+            if @gladiators[1].weapon == ("Trident")
+              @gladiators.delete_at(1)
+            elsif @gladiators[1].weapon == ("Spear")
+              @gladiators.delete_at(0)
+            end
+          end
+      end
+
+    end
+
+
+  end
